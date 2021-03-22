70c782607d770128728ecc70cad5c79c89cc2e57
fajri288 <mhammadfajri05@gmail.com>
2021-03-22 21:22:12 +0700

    latihan

---
+++ bin/quiz_dart1.dart
@@ -0,0 +1,25 @@
+import 'package:quiz_dart1/quiz_dart1.dart' as quiz_dart1;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print("Jam Masuk : ");
+  int iMasuk = int.tryParse(stdin.readLineSync());
+  print("Jam Keluar : ");
+  int iKeluar = int.tryParse(stdin.readLineSync());
+  int iLama;
+  int iBiaya;
+
+  if (iKeluar >= iMasuk) {
+    iLama = iKeluar - iMasuk;
+  } else {
+    iLama = (12 - iMasuk) + iKeluar;
+  }
+
+  if (iLama > 2) {
+    iBiaya = 2000 + ((iLama - 2) * 500);
+  } else {
+    iBiaya = 2000;
+  }
+
+  print(iBiaya);
+}
---
+++ test/quiz_dart1_test.dart
@@ -0,0 +1,8 @@
+import 'package:quiz_dart1/quiz_dart1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
